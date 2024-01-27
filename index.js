f24993a66cd30bfdb444fc5376a33c8a200e7fbb
Mahmoud Alsameen <mahmoud_abdulrahman1@outlook.com>
2024-01-27 20:32:02 +0200

    first commit, include the root index.js, package.json and docker files

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import "dotenv/config";
+import morgan from "morgan";
+import cors from "cors";
+
+const server = express();
+
+server.use(cors());
+
+if (process.env.MODE == "DEV") {
+  morgan("dev");
+  console.log(`mode : ${process.env.MODE}`);
+}
+
+//console.log(process.env)
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => console.log(`Server Started, listening to port ${port}`));
