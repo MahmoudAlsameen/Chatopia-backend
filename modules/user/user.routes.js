edb54db94afe4361000ac96e69f305d21b3e5f45
Mahmoud Alsameen <mahmoud_abdulrahman1@outlook.com>
2024-02-07 18:50:27 +0200

    add update route and controller to user

--- modules/user/user.routes.js
+++ modules/user/user.routes.js
@@ -1,6 +1,6 @@
 import exress from "express"
-import { userRegister, userLogin } from "./user.controller.js"
-import { userRegisterValidationSchema, userLoginValidationSchema } from "./user.validation.js"
+import { userRegister, userLogin, userUpdate } from "./user.controller.js"
+import { userRegisterValidationSchema, userLoginValidationSchema, userUpdateValidationSchema } from "./user.validation.js"
 import validation from "../../middleware/validation.js"
 import auth from "../../middleware/auth.js"
 
@@ -9,7 +9,7 @@ const userRoutes = exress.Router()
 
 userRoutes.post("/register", validation(userRegisterValidationSchema), userRegister);
 userRoutes.post("/login", validation(userLoginValidationSchema), userLogin);
-
+userRoutes.patch("/:id",[auth,validation(userUpdateValidationSchema)], userUpdate);
 
 
 
