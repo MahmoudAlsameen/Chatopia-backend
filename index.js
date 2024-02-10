27b546a466979f5cbc51f0ef0f9a9a26909464e2
Mahmoud Alsameen <mahmoud_abdulrahman1@outlook.com>
2024-02-11 00:12:40 +0200

    fix morgan logging

--- index.js
+++ index.js
@@ -5,23 +5,19 @@ import morgan from "morgan";
 import connection from "./DB/connection.js";
 import userRoutes from "./modules/user/user.routes.js";
 
-
-
 const port = process.env.PORT || 3000;
-const mode = process.env.MODE;
+const envMorganLogging = process.env.MORGAN_LOGGING;
 const server = express();
 
 server.use(cors());
 server.use(express.json());
-server.use('/user', userRoutes);
-
-
-
-
-if (mode == "DEV") {
-  server.use(morgan("dev"));
-  console.log(`mode : ${mode}`);
+if (envMorganLogging) {
+  server.use(morgan(envMorganLogging));
+  console.log(`Morgan_Logging mode: ${envMorganLogging}`);
 }
+server.use("/user", userRoutes);
 
 connection();
-server.listen(port, () => console.log(`Server Started, listening to port ${port}`));
+server.listen(port, () =>
+  console.log(`Server Started, listening to port ${port}`)
+);
